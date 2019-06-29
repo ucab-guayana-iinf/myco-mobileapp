bfb0916e77b3ef3c19779658a858eda45c84c081
Alejandro Chacin <ajchacin1307@gmail.com>
2019-06-29 15:21:55 -0400

    [FA-12] Admin: Detalles de Pago (Incompleto)
    Vista que contendrá los datos de pago que los propietarios envía usando el "Formulario de Pago"

--- src/screens/DetallesPago.js
+++ src/screens/DetallesPago.js
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { 
   Text, 
-  View, 
+  View,
+  ScrollView, 
   StyleSheet,
   TouchableOpacity,
   Image,
 } from 'react-native';
 import {DrawerActions} from 'react-navigation';
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 import Modal from '../components/Modal'
+import { Card } from 'react-native-elements';
+import Estado from '../components/ModalEstado'
+
+var color = 'red';
+var valor = 'Rechazado';
 
 export default class DetallesPago extends React.Component {
   Header = (
@@ -27,11 +33,63 @@ export default class DetallesPago extends React.Component {
     </View> 
   );
   
+  renderCard = (text, iconname, color, valor) => (
+    <View style={styles.container}>
+      <Card>
+        <View style={{ flexDirection: 'row' }}>
+          <Ionicons name={iconname} size={23} color={color} />
+          <Text style={styles.paragraph1}>
+             {text}
+          </Text>
+          <Text style={styles.paragraph2}>
+             {valor}
+          </Text>
+        </View>       
+      </Card>
+    </View>
+  );
+  
   render() {
     return (
      <View>
         {this.Header}
+        <ScrollView>
+          {this.renderCard('Propietario', 'ios-person', '#1bb98f', 'Alejandro Chacin')}
+          {this.renderCard('Fecha', 'md-calendar', '#2F6FD6', '19/06/2019')}
+          {this.renderCard('Monto', 'logo-usd', '#1bb98f', 'Bs 50000')}
+          {this.renderCard('Concepto', 'ios-warning', '#2F6FD6', 'Condominio')}
+          {this.renderCard('Método', 'ios-card', '#1bb98f', 'Depósito')}
+          {this.renderCard('N° de Referencia', 'md-medical', '#2F6FD6', '1234')}
+          {this.renderCard('Banco Destino', 'md-business', '#1bb98f', 'Venezuela')}
+          <View style={{marginBottom: 100}}>
+            <Estado/>
+          </View>
+        </ScrollView>
      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  paragraph1: {
+    marginLeft: 20,
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#2F6FD6',
+  },
+  paragraph2: {
+    marginLeft: 20,
+    marginRight: 20,
+    fontSize: 15,
+    textAlign: 'center',
+    color: 'gray',
+  },
+});
